b96511442e8a2e678bd604f6c1677f7d85da461b
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2022-05-06 20:14:22 -0300

    feat: send mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,37 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e3c9c2e11cc2c5",
+    pass: "52ca0542b1ea47",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const feedback = await prisma.feedback.create({
     data: { type, comment, screenshot },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Leonardo do Nascimento <leonardo3822@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `<p></p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
